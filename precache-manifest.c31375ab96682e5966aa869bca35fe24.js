c81516afc7a5d96a85917464a8c1f9528eb1391f
Ketan Pawar <ketanpawar@FVFGV626Q05N.local>
2022-06-07 19:07:03 +0530

    Updates

---
+++ precache-manifest.c31375ab96682e5966aa869bca35fe24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350ae39e755b3e10cb56c1a2fe954ecb",
+    "url": "/learningReact_screen2/index.html"
+  },
+  {
+    "revision": "48f19426d4f67912d2b3",
+    "url": "/learningReact_screen2/static/css/main.75d7cb33.chunk.css"
+  },
+  {
+    "revision": "8a43e532e2d2c4e03c33",
+    "url": "/learningReact_screen2/static/js/2.9d9d6fd1.chunk.js"
+  },
+  {
+    "revision": "da11baabf64dfea4c5dd064cac47732e",
+    "url": "/learningReact_screen2/static/js/2.9d9d6fd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f19426d4f67912d2b3",
+    "url": "/learningReact_screen2/static/js/main.6990d98a.chunk.js"
+  },
+  {
+    "revision": "11ab0b6facda868116d9",
+    "url": "/learningReact_screen2/static/js/runtime-main.aae63409.js"
+  }
+]);
